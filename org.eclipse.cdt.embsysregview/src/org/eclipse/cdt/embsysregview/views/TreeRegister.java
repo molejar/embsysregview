7b4badccb56afee18de49a58c5ddf0a5aa35b475
ravenclaw78 <ravenclaw78@da52208a-6239-4231-b5d4-a1c0043809fd>
2015-03-16 21:46:50 +0000

    Preparation for 0.2.5 GPL -> EPL switch

--- org.eclipse.cdt.embsysregview/src/org/eclipse/cdt/embsysregview/views/TreeRegister.java
+++ org.eclipse.cdt.embsysregview/src/org/eclipse/cdt/embsysregview/views/TreeRegister.java
@@ -1,18 +1,13 @@
-// This file is part of EmbSysRegView.
-//
-// EmbSysRegView is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// EmbSysRegView is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with EmbSysRegView.  If not, see <http://www.gnu.org/licenses/>.
-
+/*******************************************************************************
+ * Copyright (c) 2015 EmbSysRegView
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     ravenclaw78 - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.cdt.embsysregview.views;
 
 import org.eclipse.core.runtime.IAdaptable;
